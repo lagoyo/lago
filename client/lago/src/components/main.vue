e795e2bc256499983458e27e5e0c0c4c015c62a8
Gihyun YUN <ddumbugie@gmail.com>
2020-06-25 12:55:08 +0900

    최초 클라이언트 개발 환경 구성
    - vue.js 클라이언트 개발 환경 구성
    - server 개발 디렉토리 빈 상태로 유지

---
+++ client/lago/src/components/main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main">
+    <div>Lago Project</div>
+    <div class="d1">{{ disc }}</div>
+    <div class="d2">{{ second }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    disc: String,
+    second: Number
+  }
+}
+</script>
+
+<style scoped>
+  .main {
+    margin: 40px;
+  }
+
+  .d1 {
+    font-size: 20px;
+  }
+
+  .d2 {
+    color: red;
+  }
+</style>
